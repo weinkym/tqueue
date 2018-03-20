833bc0a35c12e8151f7b5796b5f9c652da493686
miaozw <miaozw@yunxuetang.cn>
2018-03-20 08:25:03 +0800

    增加c++11支持

--- bin/test_script.sh
+++ bin/test_script.sh
@@ -2,18 +2,23 @@ basepath=$(cd `dirname $0`; pwd)
 echo basepath=${basepath}
 cd ${basepath}
 
+LOG_FILE_PATH=${basepath}/log.txt
+
 CHECK_FILE_PATH_LIST+=(${basepath}/program1)
 CHECK_FILE_PATH_LIST+=(${basepath}/program2)
+
+echo "===" | tee ${LOG_FILE_PATH}
 
 P_CHECK_PARAM=-f
 for P_CHECK_PATH in ${CHECK_FILE_PATH_LIST[@]}; do
 	if [ ! $P_CHECK_PARAM "$P_CHECK_PATH" ]; then
-		echo "$P_CHECK_PATH is not exist"
+		echo "$P_CHECK_PATH is not exist" | tee -a ${LOG_FILE_PATH}
 		exit
 	else
-		echo "$P_CHECK_PATH is ok"
+		echo "$P_CHECK_PATH is ok" | tee -a ${LOG_FILE_PATH}
 	fi
 done
+
 
 #程序运行的时间
 
@@ -23,15 +28,15 @@ done
 # IN_PROGRAM_NAME_LIST+=(program2)
 
 #重复测试次数
-IN_REPEAT_TIMES=10
+IN_REPEAT_TIMES=30
 
 #测试时间，单位：秒
 IN_TEST_SECS_LIST+=(1)
 IN_TEST_SECS_LIST+=(5)
 IN_TEST_SECS_LIST+=(10)
-# IN_TEST_SECS_LIST+=(30)
-# IN_TEST_SECS_LIST+=(60)
-
+IN_TEST_SECS_LIST+=(30)
+IN_TEST_SECS_LIST+=(60)
+IN_TEST_SECS_LIST+=(300)
 
 for secs in ${IN_TEST_SECS_LIST[@]}; do
 	result1=0
@@ -39,17 +44,17 @@ for secs in ${IN_TEST_SECS_LIST[@]}; do
 
 	for ((i=1; i<=${IN_REPEAT_TIMES}; i ++)); do  
 		REST=`./program1 ${secs} | grep -o '[0-9]\+'`
-		echo program1运行${secs}秒,完成次数=${REST}
+		echo program1运行${secs}秒,完成次数=${REST} | tee -a ${LOG_FILE_PATH}
 		result1=$[$REST+$result1]
 	done
 	for ((i=1; i<=${IN_REPEAT_TIMES}; i ++)); do  
 		REST=`./program2 ${secs} | grep -o '[0-9]\+'`
-		echo program2运行${secs}秒,完成次数=${REST}
+		echo program2运行${secs}秒,完成次数=${REST} | tee -a ${LOG_FILE_PATH}
 		result2=$[$REST+$result2]
 	done  
 	# rate=$(echo "${result1}*1.0/${result2}"|bc)
 	rate=$(echo "scale=4;${result1}/${result2}" | bc)
-	echo 运行${secs}秒 program1 平均次数=$[$result1/$IN_REPEAT_TIMES],program2 平均次数=$[$result2/$IN_REPEAT_TIMES] 比例：${rate}
+	echo 运行${secs}秒 program1 平均次数=$[$result1/$IN_REPEAT_TIMES],program2 平均次数=$[$result2/$IN_REPEAT_TIMES] 比例：${rate} | tee -a ${LOG_FILE_PATH}
 
 done
 
